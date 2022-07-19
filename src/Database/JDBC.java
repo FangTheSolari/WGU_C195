852f1b38506022e131aac6a7e51cc1601b605718
FangTheSolari <arutla2@wgu.edu>
2022-07-19 14:31:17 -0700

    fourth save

---
+++ src/Database/JDBC.java
@@ -0,0 +1,32 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class JDBC {
+    private static final String proto = "jdbc";
+    private static final String vend = ":mysql:";
+    private static final String local="//localhost:3306/";
+    private static final String databaseName="client_schedule";
+    private static final String DB_URL=proto+vend+local+databaseName+"?connectionTimeZone=SERVER";
+    private static final String username="sqlUser";
+    private static final String password="Passw0rd!";
+    private static Connection connect;
+
+    public static void openConnection() {
+        try{
+            connect= DriverManager.getConnection(DB_URL,username,password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void closeConnection() {
+        try {
+            connect.close()
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
--- src/Model/Main.java
+++ src/Model/Main.java
@@ -6,18 +6,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
+
+    public static void main(String[] args){
+        JDBC.openConnection();
+        launch(args);
+        JDBC.closeConnection();
+    }
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"));
+        ResourceBundle language = ResourceBundle.getBundle("Resource/Language", Locale.getDefault());
+        primaryStage.setTitle("Schedule Application");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
